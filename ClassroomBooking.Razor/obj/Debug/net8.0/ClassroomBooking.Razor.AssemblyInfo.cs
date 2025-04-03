113d66cdc6a48664133f199cfd0622b555044cd1
huy69185 <huyh69185@gmail.com>
2025-04-03 17:55:21 +0700

    fix front end create

--- ClassroomBooking.Razor/obj/Debug/net8.0/ClassroomBooking.Razor.AssemblyInfo.cs
+++ ClassroomBooking.Razor/obj/Debug/net8.0/ClassroomBooking.Razor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassroomBooking.Razor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce29b910a5a87cdcd407f9ad0efd5b5fac0cc715")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eaa1e56051fe73061c9662302bfad780bfb55316")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassroomBooking.Razor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassroomBooking.Razor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
